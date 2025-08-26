e2a5a03667b95ba2971aafbcd226333a67639c44
niskan516.dev <sinhong2011@gmail.com>
2025-08-26 16:26:49 +0800

    chore(config): tweak biome rules, metro minifier, and EAS profiles

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,10 @@ const config = getDefaultConfig(__dirname);
 
 // Add .po files to asset extensions so they can be imported
 config.resolver.assetExts.push('po');
+config.transformer.minifierPath = require.resolve('metro-minify-esbuild');
+
+config.transformer.minifierConfig = {
+  drop: ['console'],
+};
 
 module.exports = config;
